5dfd619eaf4f5e1bd46f533c3e577252dc838449
Beinan Li <li.beinan@gmail.com>
2024-04-13 16:48:20 -0400

    'cleanup'

--- kkpyai/kktorch.py
+++ kkpyai/kktorch.py
@@ -251,7 +251,6 @@ class ClassifierModel(Model):
             # plot predictions
             self.plot.unblock()
             self.plot_decision_boundary(train_set)
-            breakpoint()
             self.plot.plot_learning(losses['train'], losses['test'])
         # final test predictions
         return pred
